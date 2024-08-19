6c85d9e77700015bc1bc0d257dbf8dfa7a2be9fe
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2024-08-18 22:17:46 -0300

    updates

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
-import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
 
   void api.post.getLatest.prefetch();
 
@@ -45,21 +43,9 @@ export default async function Home() {
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
             </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
           </div>
 
-          {session?.user && <LatestPost />}
+          <LatestPost />
         </div>
       </main>
     </HydrateClient>
